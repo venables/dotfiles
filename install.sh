293d55a9d435bcf48406fee7f0e3b2b07f8e8e90
Matt Venables <mattvenables@gmail.com>
2017-04-18 15:55:00 -0400

    Add vim-plug

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# get the root directory of this repo
+# Get the root directory of this repo
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# symlink all dotfiles to ~
+# Symlink all dotfiles to ~/
 ln -sf $DIR/.agignore ~/.agignore
 ln -sf $DIR/.gitconfig ~/.gitconfig
 ln -sf $DIR/.hushlogin ~/.hushlogin
@@ -11,7 +11,10 @@ ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $DIR/.vimrc ~/.config/nvim/init.vim
 ln -sf $DIR/.zshrc ~/.zshrc
 
-# symlink bin files
+# Symlink bin files
 ln -sf $DIR/bin/db /usr/local/bin/db
 ln -sf $DIR/bin/git-cleanup /usr/local/bin/git-cleanup
 ln -sf $DIR/bin/git-sync /usr/local/bin/git-sync
+
+# Install plug-vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
