f9cfa22ac8dc3cd403b3424e549f2019322f9ae3
Matt Venables <mattvenables@gmail.com>
2017-04-18 13:46:41 -0400

    Add .hushlogin file to prevent 'Last login' message

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # symlink all dotfiles to ~
 ln -sf $DIR/.gitconfig ~/.gitconfig
+ln -sf $DIR/.hushlogin ~/.hushlogin
 ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $DIR/.zshrc ~/.zshrc
 
 # symlink bin files
-ln -s $DIR/bin/db /usr/local/bin/db
-ln -s $DIR/bin/git-cleanup /usr/local/bin/git-cleanup
-ln -s $DIR/bin/git-sync /usr/local/bin/git-sync
+ln -sf $DIR/bin/db /usr/local/bin/db
+ln -sf $DIR/bin/git-cleanup /usr/local/bin/git-cleanup
+ln -sf $DIR/bin/git-sync /usr/local/bin/git-sync
