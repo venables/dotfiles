2a146e2c79ecf9fd6ec3a7c292b06f7bbaa268d6
Matt Venables <mattvenables@gmail.com>
2017-04-18 13:35:45 -0400

    Add bin folder

--- install.sh
+++ install.sh
@@ -7,3 +7,8 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 ln -sf $DIR/.gitconfig ~/.gitconfig
 ln -sf $DIR/.vimrc ~/.vimrc
 ln -sf $DIR/.zshrc ~/.zshrc
+
+# symlink bin files
+ln -s $DIR/bin/db /usr/local/bin/db
+ln -s $DIR/bin/git-cleanup /usr/local/bin/git-cleanup
+ln -s $DIR/bin/git-sync /usr/local/bin/git-sync
