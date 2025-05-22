5f46cc382576ff946199f1090c1f2f4273879ce8
Matt Venables <matt@venabl.es>
2025-05-22 09:54:16 -0400

    Add git config scripts

---
+++ scripts/git.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+# Set git configuration
+git config --global user.name "Matt Venables"
+git config --global user.email "matt@venabl.es"
+git config --global gpg.program $(which gpg)
