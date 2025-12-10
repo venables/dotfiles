e561d780e23d084067127c6dc7b1e01ca4470b62
Matt Venables <matt@venabl.es>
2025-12-10 04:33:06 -0500

    Add mini-surround and use cs like tpope surround

---
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,57 @@
+-- Disable c-s for flash.nvim, insetad remap to
+-- <leader>j, <leader>t, <leader>r, <leader>T
+-- https://github.com/LazyVim/LazyVim/discussions/1299#discussioncomment-7492298
+return {
+  {
+    "folke/flash.nvim",
+    enabled = true,
+    keys = {
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "o", "x" }, false },
+      { "r", mode = "o", false },
+      { "R", mode = { "o", "x" }, false },
+      {
+        "<leader>j",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "<leader>t",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "<leader>r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "<leader>T",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+    },
+  },
+
+  {
+    "nvim-mini/mini.surround",
+    enabed = true,
+    opts = {
+      mappings = {
+        replace = "cs",
+      },
+    },
+  },
+}
