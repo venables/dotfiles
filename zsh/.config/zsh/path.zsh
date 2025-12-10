b155a0fb0741f87bc5bcd7131bae280a5beeb932
Matt Venables <matt@venabl.es>
2025-12-10 16:11:58 -0500

    Add psql binary to PATH

--- zsh/.config/zsh/path.zsh
+++ zsh/.config/zsh/path.zsh
@@ -4,6 +4,7 @@ typeset -U path  # unique entries only
 path=(
   "$HOME/.opencode/bin"           # opencode CLI
   "$HOME/.local/bin"              # user-local binaries
+  "$HOMEBREW_PREFIX/opt/libpq/bin" # psql command
   "$HOME/.local/share/solana/install/active_release/bin"  # solana toolchain
   $path
 )
