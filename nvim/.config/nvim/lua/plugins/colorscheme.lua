986d2551652cbdcb5861366bb951ee25b7c7ea0b
Matt Venables <matt@venabl.es>
2025-12-15 14:09:58 -0500

    TokyoNight Night theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,8 @@
 return {
   {
-    "rebelot/kanagawa.nvim",
-    opts = {
-      theme = "wave",
-    },
-  },
-
-  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "tokyonight-night",
     },
   },
 }
