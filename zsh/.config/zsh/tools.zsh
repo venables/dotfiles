bc9a6da07d3b8446e6734e46654b9e18d8aa78c3
Matt Venables <matt@venabl.es>
2025-12-15 14:14:09 -0500

    Drop atuin
    Icky feeling to sync history, even if self-hosted

--- zsh/.config/zsh/tools.zsh
+++ zsh/.config/zsh/tools.zsh
@@ -18,11 +18,6 @@ if command -v fzf &>/dev/null; then
   source <(fzf --zsh)
 fi
 
-# Atuin (shell history)
-if command -v atuin &>/dev/null; then
-  eval "$(atuin init zsh)"
-fi
-
 # Yazi (Shell Wrapper)
 function y() {
 	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
