5a02eb1cefdd148688a001990e12ca43e0c9c1d5
Matt Venables <matt@venabl.es>
2025-12-24 17:40:50 -0500

    Use oldworld theme, oled variant

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
 return {
   {
+    "dgox16/oldworld.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      variant = "oled",
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "oldworld",
     },
   },
 }
