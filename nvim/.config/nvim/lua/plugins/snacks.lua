addb9d98a6731a2909abf4ac92a8a7cf420ec895
Matt Venables <matt@venabl.es>
2026-01-03 15:06:48 -0500

    Better snacks.nvim picker/explorer logic

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -21,5 +21,22 @@ return {
         header = { "%s", align = "left", hl = "normal" },
       },
     },
+    -- Picker: use fd + ripgrep with hidden files, exclude common junk
+    picker = {
+      sources = {
+        -- Explorer: show hidden files by default
+        explorer = {
+          hidden = true,
+        },
+        files = {
+          hidden = true,
+          ignored = false, -- respect .gitignore
+        },
+        grep = {
+          hidden = true,
+          ignored = false, -- respect .gitignore
+        },
+      },
+    },
   },
 }
