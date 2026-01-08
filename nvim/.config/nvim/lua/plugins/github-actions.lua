7aacf336ef8bf85e34e96d7b5a4572ccf0128fa2
Matt Venables <matt@venabl.es>
2026-01-08 08:41:28 -0500

    Add github actions nvim

---
+++ nvim/.config/nvim/lua/plugins/github-actions.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "skanehira/github-actions.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      -- "nvim-telescope/telescope.nvim", -- Optional: for enhanced workflow selection
+    },
+    opts = {},
+  },
+}
