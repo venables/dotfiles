40e82c52cb6c014d18eeab15dcd743247909a689
Matt Venables <matt@venabl.es>
2026-01-08 10:20:27 -0500

    Better hidden file logic for explorer

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,10 @@ return {
     priority = 1000,
     opts = {
       variant = "oled",
+      highlight_overrides = {
+        -- Make untracked git files brighter (default links to NonText which is same as hidden)
+        SnacksPickerGitStatusUntracked = { fg = "#85b5ba" }, -- cyan from oldworld palette
+      },
     },
   },
   {
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -24,17 +24,52 @@ return {
     -- Picker: use fd + ripgrep with hidden files, exclude common junk
     picker = {
       sources = {
-        -- Explorer: show hidden files by default
+        -- Explorer: show hidden + ignored files, but exclude common noise
         explorer = {
           hidden = true,
+          ignored = true, -- show gitignored files like .env
+          exclude = {
+            "__pycache__",
+            ".cache",
+            ".DS_Store",
+            ".git",
+            ".mypy_cache",
+            ".next",
+            ".pytest_cache",
+            ".turbo",
+            ".venv",
+            ".vercel",
+            "build",
+            "coverage",
+            "dist",
+            "node_modules",
+            "venv",
+          },
         },
         files = {
           hidden = true,
-          ignored = false, -- respect .gitignore
+          ignored = true, -- include .env files
+          exclude = {
+            "__pycache__",
+            ".cache",
+            ".DS_Store",
+            ".git",
+            ".mypy_cache",
+            ".next",
+            ".pytest_cache",
+            ".turbo",
+            ".venv",
+            ".vercel",
+            "build",
+            "coverage",
+            "dist",
+            "node_modules",
+            "venv",
+          },
         },
         grep = {
           hidden = true,
-          ignored = false, -- respect .gitignore
+          ignored = false, -- respect .gitignore for grep (don't search in node_modules)
         },
       },
     },
